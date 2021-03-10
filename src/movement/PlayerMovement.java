a2dd6afb70682f9824b49609588eed26cbd91adc
madmanham11 <smithphilip812@gmail.com>
2021-03-09 21:04:53 -0700

    got map game strong

--- src/movement/PlayerMovement.java
+++ src/movement/PlayerMovement.java
@@ -1,21 +1,21 @@
 package movement;
 
-public class PlayerMovement extends Objects {
+//public class PlayerMovement extends Objects {
 
-    private Room room;
+//    private Room room;
 
-    public PlayerMovement(String name, String description, Room room){
-        super(name, description);
-        this.room = room;
-    }
+   // public PlayerMovement(String name, String description, Room room){
+   //     super(name, description);
+   //     this.room = room;
+   // }
 
-    public Room getRoom() {
-        return room;
-    }
+///    public Room getRoom() {
+    //    return room;
+    //}
 
-    public void setRoom(Room room) {
-        this.room = room;
-    }
+    //public void setRoom(Room room) {
+    //    this.room = room;
+    //}
 
 
-}
+//}
--- src/views/View.java
+++ src/views/View.java
@@ -1,23 +1,66 @@
 package views;
 
+import enums.Direction;
 import lib.ConsoleIO;
 import models.Enemy;
 import models.Items;
 import models.Map;
 import models.Player;
-import movement.PlayerMovement;
 
 public class View {
     public Player hero = new Player();
     public Items bag = new Items();
     public Enemy enemy = new Enemy();
     public Map map = new Map();
+//    public int pointer = ConsoleIO.promptForMenuSelection(new String[]{"North", "South", "East", "West"}, false);
     private int difficult = 1;
+    private int drinks = 1;
 
     public void mainMenu(){
         setDifficulty();
-        map.startingRoom();
-        movement();
+        map.startup();
+        System.out.println("\nWelcome to your doom.");
+        System.out.println("Which way would you like to move?\n");
+        loopRoom();
+        checkRoom();
+
+
+    }
+
+    public void checkRoom(){
+
+        switch(map.currentRoom.getName()) {
+            case "Healing Fountain":
+                healingFountain();
+                break;
+            case "Miniboss Room":
+                //make miniboss
+                commonRoom();
+                break;
+            case "Boss Room":
+                //Make boss
+                bossRoom();
+                break;
+            default:
+                movement();
+                commonRoom();
+                break;
+        }
+
+    }
+    public Boolean promptThenMove(){
+        switch (ConsoleIO.promptForMenuSelection(new String[]{"North", "South", "East", "West"}, false)){
+            case 1:
+                return map.goDirection(Direction.NORTH);
+            case 2:
+                return map.goDirection(Direction.SOUTH);
+            case 3:
+                return map.goDirection(Direction.EAST);
+            case 4:
+                return map.goDirection(Direction.WEST);
+            default:
+                return null;
+        }
     }
 
     private void setDifficulty(){
@@ -31,7 +74,7 @@ public class View {
                 enemy.setHealth(50); //PLACEHOLDER VALUE
                 enemy.setDefense(25); //PLACEHOLDER VALUE
                 hero.setAttack(20); //PLACEHOLDER VALUE
-                map.healingFountain(1);
+                drinks =3;
                 break;
             case 2:
                 //medium
@@ -39,7 +82,7 @@ public class View {
                 enemy.setHealth(75); //PLACEHOLDER VALUE
                 enemy.setDefense(50); //PLACEHOLDER VALUE
                 hero.setAttack(25); //PLACEHOLDER VALUE
-                map.healingFountain(2);
+                drinks =2;
                 break;
             case 3:
                 //hard
@@ -47,23 +90,57 @@ public class View {
                 enemy.setHealth(100); //PLACEHOLDER VALUE
                 enemy.setDefense(75); //PLACEHOLDER VALUE
                 hero.setAttack(30); //PLACEHOLDER VALUE
-                map.healingFountain(3);
+                drinks =1;
                 break;
         }
     }
 
-    private void movement(){
+    public void bossRoom(){
+        System.out.println("This is the boss room. Are you sure you're ready for this?");
+        int fin;
+        fin = ConsoleIO.promptForMenuSelection(new String[]{"Yes", "No"}, false);
+        switch (fin){
+            case 1:
+                //fight boss
+                break;
+            case 2:
+                //stay in previous room
+                break;
+        }
+
+    }
+
+    private void movement() {
 //        map.updateOutput();
-        switch(enemy.spawnChance()){
+        switch (enemy.spawnChance()) {
             case 0:
                 System.out.println("Looks like this room is empty.");
-                map.commonRoom();
+                commonRoom();
                 break;
             case 1:
                 combat();
                 break;
         }
+    }
 
+        public void commonRoom(){
+            System.out.println("You enter a common room");
+            loopRoom();
+            checkRoom();
+
+        }
+
+    public void healingFountain(){
+        //moveTo(put info in here);
+        if (drinks!= 0){
+            hero.setHealthPoints(100);
+            drinks--;
+            System.out.println("Your health has been restored. You are at " + hero.getHealthPoints() + " health.");
+        } else {
+            System.out.println("Unfortunately, you've used the healing fountain too many times. Your health is currently at " + hero.getHealthPoints() + ". Good luck out there.");
+        }
+        loopRoom();
+        checkRoom();
 
     }
 
@@ -85,5 +162,22 @@ public class View {
                 break;
 
         }
+    }
+    public void loopRoom(){
+        boolean meh;
+        Boolean burger;
+        do{
+            burger = promptThenMove();
+            if(burger == null){
+                System.out.println("no");
+                meh = false;
+            }else{
+                if (burger) {
+                    meh = false;
+                }else{
+                    meh = true;
+                }
+            }
+        }while(!meh);
     }
 }
